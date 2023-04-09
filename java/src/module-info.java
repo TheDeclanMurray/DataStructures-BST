6662c02ff507165ca2bd9ccc7712b0c016d49cca
Declan <declanm2022>
2023-04-08 21:07:19 -0700

    cleaned

--- java/src/module-info.java
+++
@@ -1,8 +0,0 @@
-module plans {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires java.desktop;
-
-    opens org.headroyce.declanm2022 to javafx.fxml;
-    exports org.headroyce.declanm2022;
-}
