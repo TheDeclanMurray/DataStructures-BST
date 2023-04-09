68379dc4593c420662449c121b29bee344c6f171
Declan <declanm2022>
2023-04-08 21:04:09 -0700

    cleaned house

--- javascript/server.js
+++
@@ -1,9 +0,0 @@
-let express = require('express')
-
-let server = express();
-server.use(express.static('app'))
-
-
-server.listen(3000, function(){
-    console.log('Server listening on port 3000');
-})
